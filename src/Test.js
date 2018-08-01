8eaf8327657a5cfe57d5952d05e4d68346f7575d
mnogueron <matthieu.nogueron@live.fr>
2018-08-01 14:34:45 +0200

    Add storybook

---
+++ src/Test.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {StyleSheet, View} from 'react-native'
+
+class Test extends Component {
+
+    render() {
+        return <View/>
+    }
+
+}
+
+const styles = StyleSheet.create({})
+
+export default Test
