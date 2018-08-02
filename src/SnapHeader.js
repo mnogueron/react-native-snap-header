cac34c7b4787f93aa908977f5822ea697625ec88
mnogueron <matthieu.nogueron@live.fr>
2018-08-02 12:04:32 +0200

    Fix snapping

--- src/SnapHeader.js
+++ src/SnapHeader.js
@@ -1,54 +1,54 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { StyleSheet, Text, View, Animated } from 'react-native'
+import { Animated, StyleSheet, View } from 'react-native'
 
-class HeaderTest extends Component {
+class SnapHeader extends React.PureComponent {
 
     state = {
         scrollY: new Animated.Value(0),
+        startDragY: 0,
     }
 
     headerOpen = true
 
-    startDragY = 0
     lastScrollToY = 0
     isScrolling = false
 
-    isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
+    _isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
         const paddingToBottom = 20
         return layoutMeasurement.height + contentOffset.y >=
            contentSize.height - paddingToBottom
     }
 
-    onScrollBeginDrag = event => this.startDragY = event.nativeEvent.contentOffset.y
+    _onScrollBeginDrag = event => this.setState({ startDragY: event.nativeEvent.contentOffset.y })
 
-    onScrollEndSnapToEdge = event => {
+    _onScrollEndSnapToEdge = event => {
 
         if (this.isScrolling) {
             this.isScrolling = false
             return
         }
 
-        const startDragY = this.startDragY
+        const { startDragY } = this.state
         const { minHeight, maxHeight, percentToClose } = this.props
         const scrollRange = maxHeight - minHeight
 
         const y = event.nativeEvent.contentOffset.y
 
         if (this._scrollView) {
-            console.log('start onScrollEndSnapToEdge', startDragY, y, this.headerOpen)
+            console.log('start _onScrollEndSnapToEdge', startDragY, y, this.headerOpen)
             let toY = null
 
             if (this.headerOpen) {
                 if (startDragY < y && y < startDragY + scrollRange * percentToClose) {
                     toY = startDragY
                 } else if (startDragY + scrollRange * percentToClose <= y && y < startDragY + scrollRange
-                   && !this.isCloseToBottom(event.nativeEvent)) {
+                   && !this._isCloseToBottom(event.nativeEvent)) {
                     toY = startDragY + scrollRange
                 }
 
                 if (y >= startDragY + scrollRange * percentToClose
-                  && !this.isCloseToBottom(event.nativeEvent)) {
+                  && !this._isCloseToBottom(event.nativeEvent)) {
                     this.headerOpen = false
                 }
             }
@@ -64,8 +64,6 @@ class HeaderTest extends Component {
                 }
             }
 
-            console.log('attempt scrolling to ' + Math.round(toY) + ' with last scrollToY ' + this.lastScrollToY)
-
             // Don't scroll infinitely to the same Y
             if (toY !== null) {
                 this.isScrolling = true
@@ -73,14 +71,13 @@ class HeaderTest extends Component {
                 this._scrollView.scrollTo({ y: Math.round(toY), animated: true })
                 console.log('scrollTo', Math.round(toY))
             }
-            console.log('stop onScrollEndSnapToEdge', this.headerOpen)
+            console.log('stop _onScrollEndSnapToEdge', this.headerOpen)
         }
     }
 
     render() {
-        const { minHeight, maxHeight, headerComponent } = this.props
-        const { scrollY } = this.state
-        const startDragY = this.startDragY
+        const { minHeight, maxHeight, headerComponent, children } = this.props
+        const { scrollY, startDragY } = this.state
         const scrollRange = maxHeight - minHeight
 
         console.log('Rerender', scrollY, startDragY)
@@ -105,14 +102,13 @@ class HeaderTest extends Component {
         }
 
         return (
-            <View style={ styles.container }>
-
+            <View style={{ flex: 1 }}>
                 <Animated.ScrollView
-                    style={ styles.scrollView }
+                    style={{ flex: 1 }}
                     ref={ scrollView => this._scrollView = scrollView ? scrollView._component : null }
-                    onScrollEndDrag={ this.onScrollEndSnapToEdge }
-                    onScrollBeginDrag={ this.onScrollBeginDrag }
-                    onMomentumScrollEnd={ this.onScrollEndSnapToEdge }
+                    onScrollEndDrag={ this._onScrollEndSnapToEdge }
+                    onScrollBeginDrag={ this._onScrollBeginDrag }
+                    onMomentumScrollEnd={ this._onScrollEndSnapToEdge }
                     onScroll={ Animated.event(
                         [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }],
                         { useNativeDriver: true }
@@ -120,21 +116,10 @@ class HeaderTest extends Component {
                     scrollEventThrottle={ 16 }
                 >
                     <View style={{ flex: 0, height: maxHeight }} />
-                    {
-                        Array.apply(null, { length: 40 }).map(Number.call, Number).map((item) => (
-                            <View
-                                key={ item }
-                                style={ [ styles.item, item % 2 === 1 && { backgroundColor: '#ffffff' } ] }
-                            >
-                                <Text>
-                                    { item }
-                                </Text>
-                            </View>
-                        ))
-                    }
+                    { children }
                 </Animated.ScrollView>
 
-                <Animated.View style={ [ styles.headerBackground, { height: maxHeight }, animateHeader ] }>
+                <Animated.View style={ [ styles.header, { height: maxHeight }, animateHeader ] }>
                     { headerComponent }
                 </Animated.View>
             </View>
@@ -142,38 +127,25 @@ class HeaderTest extends Component {
     }
 }
 
-HeaderTest.propTypes = {
+SnapHeader.propTypes = {
     minHeight: PropTypes.number,
     maxHeight: PropTypes.number,
     headerComponent: PropTypes.element,
     percentToClose: PropTypes.number,
 }
 
-HeaderTest.defaultProps = {
+SnapHeader.defaultProps = {
     minHeight: 50,
     maxHeight: 200,
     percentToClose: 0.5,
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    scrollView: {
-        flex:1,
-    },
-    headerBackground: {
+    header: {
         position: 'absolute',
         flex: 0,
         width: '100%',
     },
-    item: {
-        height: 80,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#e3e3e3',
-    },
 })
 
-export default HeaderTest
+export default SnapHeader
--- storybook/stories/snap-header/index.js
+++ storybook/stories/snap-header/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import { storiesOf } from '@storybook/react-native'
 import { SnapHeader } from 'react-native-snap-header'
+
+const styles = StyleSheet.create({
+    item: {
+        height: 80,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#e3e3e3',
+    },
+})
 
 storiesOf('SnapHeader', module)
     .add('Basic', () => (
@@ -11,5 +21,16 @@ storiesOf('SnapHeader', module)
                     <Text>Hey</Text>
                 </View>
             ) }
-        />
+        >
+            {
+                Array.apply(null, { length: 40 }).map(Number.call, Number).map((item) => (
+                    <View
+                        key={ item }
+                        style={ [ styles.item, item % 2 === 1 && { backgroundColor: '#ffffff' } ] }
+                    >
+                        <Text>{ item }</Text>
+                    </View>
+                ))
+            }
+        </SnapHeader>
     ))
